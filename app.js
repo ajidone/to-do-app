bfd83e845e6b667bb55c5391b5cfe20585d138a6
Adam Idone <ajidone@gmail.com>
2017-12-11 10:15:56 -0800

    adding form and todo list

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var onReady = function() {
   const addToDoForm = document.getElementById('addToDoForm')
   const newToDoText = document.getElementById('newToDoText')
   const toDoList = document.getElementById('toDoList')
-  const removeToDo = document.getElementById('removeToDo')
 
   addToDoForm.addEventListener('submit', event => {
       event.preventDefault()
@@ -23,20 +22,5 @@ var onReady = function() {
       toDoList.appendChild(newLi)
 
       newToDoText.value = ''
-  });
-
-  removeToDo.addEventListener('click', event => {
-      let toDoItems = toDoList.getElementsByTagName("li")
-      let numItems = toDoItems.length
-
-      for(var i = 0; i < numItems; i++) {
-        let item = toDoItems[i]
-        let itemChecked = item.getElementsByTagName('input')[0].checked
-        if (itemChecked) {
-          toDoList.removeChild(item)
-          i--
-          numItems--
-        }
-      }
   });
 }
