43d467951bc37f316d1eb87c6e51f5a956844d90
Adam Idone <ajidone@gmail.com>
2017-12-11 12:17:09 -0800

    Module 2 Checkpoint 8 initial commit, refactoring code

--- app.js
+++ app.js
@@ -3,24 +3,51 @@ window.onload = function() {
 }
 
 var onReady = function() {
+//function to create new to-do items and also render the UI
+  var createNewToDo = function () {
+    if (!newToDoText.value) { return }
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    })
+
+    newToDoText.value = ''
+
+    renderTheUI()
+  }
+
+//function to rebuild the UI with current state
+  var renderTheUI = function () {
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+        const newLi = document.createElement('li')
+
+        const checkbox = document.createElement('input')
+        checkbox.type = "checkbox"
+
+        const title = document.createElement('span')
+        title.textContent = toDo.title
+
+        toDoList.appendChild(newLi)
+        newLi.appendChild(title)
+        newLi.appendChild(checkbox)
+    })
+  }
+
+//Main program code
+  let toDos = []
+
   const addToDoForm = document.getElementById('addToDoForm')
   const newToDoText = document.getElementById('newToDoText')
   const toDoList = document.getElementById('toDoList')
 
   addToDoForm.addEventListener('submit', event => {
-      event.preventDefault()
+    event.preventDefault()
 
-      let title = newToDoText.value
-      let newLi = document.createElement('li')
-      let checkbox = document.createElement('input')
+    createNewToDo()
+  })
 
-      checkbox.type = "checkbox"
-
-      newLi.textContent = title
-      newLi.appendChild(checkbox)
-
-      toDoList.appendChild(newLi)
-
-      newToDoText.value = ''
-  });
+  renderTheUI()
 }
