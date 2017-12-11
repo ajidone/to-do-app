8b448fd6e651e91a9e8039046d4cd1813724213d
Adam Idone <ajidone@gmail.com>
2017-12-11 15:38:24 -0800

    Module 2 Assignment 8 initial commit

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ var onReady = function() {
   var createNewToDo = function () {
     if (!newToDoText.value) { return }
 
+    id++
+
     toDos.push({
+      id: id,
       title: newToDoText.value,
       complete: false
     })
@@ -15,6 +18,22 @@ var onReady = function() {
     newToDoText.value = ''
 
     renderTheUI()
+    saveToLocalStore(toDos)
+  }
+
+//function to remove to do items from array and rebuild UI
+  var removeToDo = function (btnId) {
+    let id = btnId.replace('deleteBtn','')
+
+    toDos = toDos.filter(toDo => toDo.id != id)
+
+    renderTheUI()
+    saveToLocalStore(toDos)
+  }
+
+//function to save To Dos array to local storage at end of create or remove
+  var saveToLocalStore = function (toDos) {
+   localStorage.setItem("ToDos", JSON.stringify(toDos))
   }
 
 //function to rebuild the UI with current state
@@ -23,6 +42,7 @@ var onReady = function() {
 
     toDos.forEach(function(toDo) {
         const newLi = document.createElement('li')
+        newLi.id = "toDo" + toDo.id
 
         const checkbox = document.createElement('input')
         checkbox.type = "checkbox"
@@ -30,18 +50,32 @@ var onReady = function() {
         const title = document.createElement('span')
         title.textContent = toDo.title
 
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = "Remove"
+        deleteButton.id = "deleteBtn" + toDo.id
+        deleteButton.addEventListener('click', event => {
+          removeToDo(event.target.id)
+        })
+
         toDoList.appendChild(newLi)
         newLi.appendChild(title)
         newLi.appendChild(checkbox)
+        newLi.appendChild(deleteButton)
     })
   }
 
 //Main program code
-  let toDos = []
+  let toDos = (JSON.parse(localStorage.getItem("ToDos")) || [])
+  let id = 0
+
+  if (toDos.length > 0) {
+    id  = Math.max(...toDos.map(toDo => toDo.id))
+  }
 
   const addToDoForm = document.getElementById('addToDoForm')
   const newToDoText = document.getElementById('newToDoText')
   const toDoList = document.getElementById('toDoList')
+  const deleteToDoButton = document.querySelector('.deleteButton')
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault()
